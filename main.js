ada31cfc46cc0b8648dd387e26246487ed14ef23
Blackbeard guy <109026189+blackbeard-guy@users.noreply.github.com>
2022-10-17 22:42:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+async function main() {
+    const postsData = await getData()
+    let currentPage = 1
+    let rows = 10 
+
+    function displayList(arrData, rowPerPage, page) {
+        const postsEl = document.querySelector('.posts')
+        postsEl.innerHTML = ''
+
+        const start = rowPerPage * page - rowPerPage
+        const end = start + rowPerPage
+        const paginatedData = arrData.slice(start, end)
+
+        paginatedData.forEach(el => {
+            const postEl = document.createElement('div')
+            postEl.classList.add('post')
+            postEl.innerText = `${el.title}`
+            postsEl.appendChild(postEl)
+        });
+    }
+    function displayPagination(arrData, rowPerPage) {
+        const paginationEl = document.querySelector('.pagination')
+        const pagesCount = Math.ceil(arrData.length / rowPerPage)   // ceil округляет в большую сторону     
+        const ulEl = document.createElement('ul')
+        ulEl.classList.add('pagination__list')
+
+        for (i = 0; i < pagesCount; i++ ) {
+            const liEl = displayPaginationBtn(i + 1)
+            ulEl.appendChild(liEl)
+        }
+
+        paginationEl.appendChild(ulEl)
+    }
+
+    function displayPaginationBtn(page) {
+        const liEl = document.createElement('li')
+        liEl.classList.add('pagination__item')
+        liEl.innerText = page
+
+        if(currentPage == page) liEl.classList.add('pagination__item--active')
+
+        liEl.addEventListener('click', () => {
+            currentPage = page
+            displayList(postsData, rows, currentPage)
+
+            const currentItemLi = document.querySelector('li.pagination__item--active')
+            currentItemLi.classList.remove('pagination__item--active')
+
+            liEl.classList.add('pagination__item--active')
+        })
+
+        return liEl
+    }
+
+    displayList(postsData, rows, currentPage)
+    displayPagination(postsData, rows)
+}
+
+main()
